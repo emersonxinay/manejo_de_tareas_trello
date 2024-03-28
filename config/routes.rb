6b0a93e1ecb0fa67c2d55d4774b4debb8737f35f
emerson <xinayespinoza@gmail.com>
2024-03-27 19:17:53 -0500

    feat: devise, routes y modelos con controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  devise_for :users, controllers: {
+    sessions: 'users/sessions'
+  }
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+  # Ruta por defecto para home#index
+  root 'home#index'
 
-  # Defines the root path route ("/")
-  # root "posts#index"
+  # Otras rutas de tu aplicación...
 end
